5735f79e6f1037793830803bcaa209648a86f7a6
lehMaxi <lehmann.max95@gmail.com>
2026-07-23 17:53:48 +0200

    Cat goes mew I guess

--- numbar4/ex00/sources/Cat.cpp
+++ numbar4/ex00/sources/Cat.cpp
@@ -8,10 +8,24 @@ Cat::Cat()
 
 Cat::Cat(Cat const &src)
 {
-if (this!=
+std::cout << "A freshly baked kitty" << std::endl;
+*this = src;
 }
 
 Cat::~Cat()
 {
 std::cout << "And here I thought there should be 8 lives left..." << std::endl;
+}
+
+Cat Cat:: operator=(Cat &template)
+{
+if (*this != template)
+std::cout << "Looks just like her mommy." << std::endl;
+this->setType(template.getType());
+return this;
+}
+
+void Cat::makeSound()
+{
+std::cout << "Cat: I guess mewmewmew would be appropriate, yes?" << std::endl;
 }
