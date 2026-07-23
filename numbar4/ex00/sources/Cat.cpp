8a38708db44531272efa825da1b30c7bca6b645f
lehMaxi <lehmann.max95@gmail.com>
2026-07-23 09:36:34 +0200

    Update Cat.cpp

--- numbar4/ex00/sources/Cat.cpp
+++ numbar4/ex00/sources/Cat.cpp
@@ -5,3 +5,8 @@ Cat::Cat()
     std::cout << "Look at the new cute kitty" << std::endl;
     _type = "Cat";
 }
+
+Cat::~Cat()
+{
+std::cout << "And here I thought there should be 8 lives left..." << std::endl;
+}
