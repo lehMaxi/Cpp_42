c2910d077cf8f04d0a3be37352732ac1d49cd603
lehMaxi <lehmann.max95@gmail.com>
2026-07-25 17:56:07 +0200

    Update Dog.cpp

--- numbar4/ex00/sources/Dog.cpp
+++ numbar4/ex00/sources/Dog.cpp
@@ -1,1 +1,31 @@
 #include "Dog.hpp"
+
+Dog::Dog()
+{
+    std::cout << "Look at the new cute puppy" << std::endl;
+    _type = "Dog";
+}
+
+Dog::Dog(Dog const &src)
+{
+std::cout << "A freshly baked puppy" << std::endl;
+*this = src;
+}
+
+Dog::~Dog()
+{
+std::cout << "Did.." << std::endl;
+}
+
+Cat Cat:: operator=(Cat &template)
+{
+if (*this != template)
+std::cout << "Looks just like her mommy." << std::endl;
+this->setType(template.getType());
+return this;
+}
+
+void Cat::makeSound()
+{
+std::cout << "Cat: I guess mewmewmew would be appropriate, yes?" << std::endl;
+}
