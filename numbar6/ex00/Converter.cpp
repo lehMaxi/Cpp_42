9ccaec864938a34b8d6d6f3e323ee1d6b4ee2159
lehMaxi <lehmann.max95@gmail.com>
2026-08-24 12:46:59 +0200

    Implement conversion print functions for types

--- numbar6/ex00/Converter.cpp
+++ numbar6/ex00/Converter.cpp
@@ -10,4 +10,51 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Converter.hpp"
 
+static void isCharPrint(std::string param)
+{
+  std::cout << "char: " << param[0] << std::endl;
+  std::cout << "int: " << static_cast<int>(param[0]) << std::endl;
+  std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(param[0]) << "f" << std::endl;
+  std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(param[0]) << std::endl;
+}
+
+static void isIntPrint(std::string param)
+{
+  int n = atoi(param.c_str());
+  
+  if ( n < 32 || n > 126)
+    std::cout << "char: " << "not possible to display" << std::endl;
+  else
+    std::cout << "char: " << static_cast<char>(n) << std::endl;
+  std::cout << "int: " << n << std::endl;
+  std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(n) << "f" << std::endl;
+  std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(n) << std::endl;
+}
+
+static void isFloatPrint(std::string param)
+{
+  float n = atof(param.c_str());
+  
+  if ( n < 32 || n > 126)
+    std::cout << "char: " << "not possible to display" << std::endl;
+  else
+    std::cout << "char: " << static_cast<char>(n) << std::endl;
+  std::cout << "int: " << static_cast<int>(n) << std::endl;
+  std::cout << "float: " << std::fixed << std::setprecision(2) << n << "f" << std::endl;
+  std::cout << "double: " << std::fixed << std::setprecision(2) << static_cast<double>(n) << std::endl;
+}
+
+static void isDoublePrint(std::string param)
+{
+  double n = atof(param.c_str());
+  
+  if ( n < 32 || n > 126)
+    std::cout << "char: " << "not possible to display" << std::endl;
+  else
+    std::cout << "char: " << static_cast<char>(n) << std::endl;
+  std::cout << "int: " << static_cast<int>(n) << std::endl;
+  std::cout << "float: " << std::fixed << std::setprecision(2) << static_cast<float>(n) << "f" << std::endl;
+  std::cout << "double: " << std::fixed << std::setprecision(2) << n << std::endl;
+}
