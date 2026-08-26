f246804bb88634ac43710aab7573fa047509e37d
lehMaxi <lehmann.max95@gmail.com>
2026-08-26 12:12:23 +0200

    I should get an Intern too

---
+++ numbar5/ex03/AForm.hpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AForm.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlehmann <mlehmann@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/14 13:34:03 by mlehmann          #+#    #+#             */
+/*   Updated: 2026/08/17 12:36:03 by mlehmann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_H
+#define FORM_H
+
+#include "Bureaucrat.hpp"
+
+class AForm
+{
+	public:
+
+		AForm(str name, int toSign, int toExecute);
+		AForm(AForm const &src);
+		virtual ~AForm();
+
+		AForm & operator=(AForm  const &rSym);
+
+		void			beSigned(Bureaucrat &signer);
+		void			execute(Bureaucrat const &executor);
+		virtual void	call() = 0;
+
+		void	setName(str name);
+		void	setSigned(bool sign);
+		void	setToSign(int grade);
+		void	setToExecute(int grade);
+
+		str		getName() const;
+		bool	getSigned() const;
+		int		getToSign() const;
+		int		getToExecute() const;
+
+		class GradeToHighException : public std::exception
+		{
+			public:
+				virtual const char*	what() const throw();
+		};
+		class GradeToLowException : public std::exception
+		{
+			public:
+				virtual const char*	what() const throw();
+		};
+		
+	protected:
+		str		_target;
+		str 	_name;
+		bool	_signed;
+		int		_toSign;
+		int		_toExecute;
+};
+
+std::ostream &	operator<<(std::ostream & o, AForm const &rSym);
+
+#endif
--- numbar5/ex03/Intern.cpp
+++ numbar5/ex03/Intern.cpp
@@ -1,31 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   PresidentialPardonForm.hpp                         :+:      :+:    :+:   */
+/*   Intern.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mlehmann <mlehmann@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/08/17 13:05:56 by mlehmann          #+#    #+#             */
-/*   Updated: 2026/08/22 11:07:41 by mlehmann         ###   ########.fr       */
+/*   Created: 2026/08/22 12:03:05 by mlehmann          #+#    #+#             */
+/*   Updated: 2026/08/22 12:03:08 by mlehmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PARDON_H
-#define PARDON_H
 
-#include "AForm.hpp"
-
-class PresidentialPardonForm: public AForm
-{
-	public:
-		PresidentialPardonForm(str target);
-		PresidentialPardonForm(PresidentialPardonForm const &src);
-		~PresidentialPardonForm();
-
-		PresidentialPardonForm & operator=(PresidentialPardonForm const &rSym);
-
-		void	call();
-		void	pardon(str place);
-};
-
-#endif
