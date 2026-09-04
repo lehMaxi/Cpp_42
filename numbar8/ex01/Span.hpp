ea7be2cdc163ca7b1c17e13fc05a10d16a3816c5
lehMaxi <lehmann.max95@gmail.com>
2026-09-04 10:56:02 +0200

    Add Span class with exception handling

--- numbar8/ex01/Span.hpp
+++ numbar8/ex01/Span.hpp
@@ -10,4 +10,57 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SPAN_H
+#define SPAN_H
 
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stdexcept>
+#include<climits>
+#include<ctime>
+#include<cstdlib>
+#include<algorithm>
+#include<unistd.h>
+
+typedef	std::string str;
+
+class Span
+{
+	public:
+		Span();
+		Span(unsigned int N);
+		Span(Span const &src);
+		~Span();
+
+		Span & operator=(Span const &src);
+
+		void	addNumber(int n);
+		void	bigAdd(int n);
+		int		shortestSpan();
+		int		longestSpan() const;
+
+		class MaxNumbersException : public std::exception
+		{
+			public:
+				virtual const char *	what() const throw();
+		};
+		class NoNumbersException : public std::exception
+		{
+			public:
+				virtual const char *	what() const throw();
+		};
+		class OneNumbersException : public std::exception
+		{
+			public:
+				virtual const char *	what() const throw();
+		};
+	private:
+		std::vector<int>	_array;
+		unsigned int		_maxSize;
+		unsigned int		_size;
+
+		std::vector<int>	_sort() const
+};
+
+#endif
