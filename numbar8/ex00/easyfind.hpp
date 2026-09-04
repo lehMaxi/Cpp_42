0b4f89c5e3ccb5229b4cff522770fd6a925f9558
lehMaxi <lehmann.max95@gmail.com>
2026-09-04 10:36:13 +0200

    Implement NoValueException in easyfind.hpp
    Added a NoValueException class for error handling in easyfind.

--- numbar8/ex00/easyfind.hpp
+++ numbar8/ex00/easyfind.hpp
@@ -10,4 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASYFIND_H
+#define EASYFIND_H
 
+#include <iostream>
+#include <string>
+#include <algorythm>
+#include <list>
+#include <stdexcept>
+
+typedef std::string str;
+
+class NoValueException : public std::exception
+{
+	public:
+		virtual const char *  what() const throw();
+};
+
+template<typename T>
+int	easyfind(T const & array, int val);
+
+#endif
