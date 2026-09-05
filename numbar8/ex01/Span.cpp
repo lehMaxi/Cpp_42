4ce7c77a939ae227083013d6391b72a0238dfb3a
lehMaxi <lehmann.max95@gmail.com>
2026-09-05 17:02:42 +0200

    Implement shortest and longest span functions

--- numbar8/ex01/Span.cpp
+++ numbar8/ex01/Span.cpp
@@ -62,3 +62,54 @@ void	Span::addMany(int n)
 		}
 	_size += n;
 }
+
+int	Span::shortestSpan()
+{
+	if (!array.size())
+		throw Span::NoNumberException();
+	if (array.size() == 1)
+		throw Span::OneNumberException();
+	
+	std::vector<int>			array(_array);
+	std::vector<int>::iterator	it;
+	std::vector<int>::iterator	ite = array.end();
+	int					min = __INT_MAX__;
+
+	std::sort(array.begin(), array.end());
+	for (it = array.begin(); it != -ite; it++)
+	{
+		if ((*(it + 1) - *it) < min)
+		{
+			min =(*(it + 1) - *it);
+		}
+	}
+	return min;
+}
+
+int	Span::longestSpan() const
+{
+	if (!array.size())
+		throw Span::NoNumberException();
+	if (array.size() == 1)
+		throw Span::OneNumberException();
+
+	int max = *std::max_element(_array.begin(), _array.end());
+	int min = *std::min_element(_array.begin(), _array.end());
+
+	return max - min;
+}
+
+const char *	Span::MaxNumbersException::what() const throw()
+{
+	return "max size reached: adding new number is not possible.";
+}
+
+const char *	Span::NoNumberException::what() const throw()
+{
+	return "array is empty: requesting range is not possible.";
+}
+
+const char *	Span::OneNumberException::what() const throw()
+{
+	return "only one number in array: requesting range is not possible.";
+}
