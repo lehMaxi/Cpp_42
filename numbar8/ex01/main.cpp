d221b5014b183e6967bca7e0c61705551d82b0af
lehMaxi <lehmann.max95@gmail.com>
2026-09-05 17:19:10 +0200

    Implement main function to test Span class

--- numbar8/ex01/main.cpp
+++ numbar8/ex01/main.cpp
@@ -10,5 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Span.hpp"
 
+int main()
+{
+	Span	array(100);
 
+	try
+	{
+		array.addMultiple(99);
+	}
+	catch(const std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << array.longestSpan() << std::endl;
+	std::cout << array.shortestSpan() << std::endl;
+
+	return 0;
+}
