5c911119a2cf52a59d8501cc9877614100857df0
lehMaxi <lehmann.max95@gmail.com>
2026-09-17 14:40:05 +0200

    Implement copy constructor, assignment operator, and print
    Added copy constructor and assignment operator for BitcoinExchange class, along with a print function to display exchange data.

--- numbar9/ex00/BitcoinExchange.cpp
+++ numbar9/ex00/BitcoinExchange.cpp
@@ -23,4 +23,49 @@ BitcoinExchange::BitcoinExchage()
 	_parseData(data);
 	_inputSize = _getInputSize(file) - 1;
 	_input = new int[_inputSize];
+	while (i <  _inputSize)
+	{
+		_input[i] = 0;
+		i++;
+	}
+	file.open(av[1], std::ifstream::in);
+	_parseInput(file);
+	file.open(av[1], std::ifstream::in);
+	_output(file);
 }
+
+BitcoinExchange::BitcoinExchage() 
+{
+	_map = src._map;
+	_inputSize = src._inputSize;
+	_input = src._input;
+}
+
+BitcoinExchange::~BitcoinExchage()
+{
+	delete [] _input;
+}
+
+BitcoinExchange & BitcoinExchange::operator=(BitcoinExchange const &src)
+{
+	if (this != &src)
+	{
+		_map = src._map;
+		_inputSize = src._inputSize;
+		_input = src._input;
+	}
+	return *this;
+}
+
+void	BitcoinExchange::print() const
+{
+	std::map<str, float>::const_iterator	mapIt = _map.begin();
+	std::map<str, float>::const_iterator	mapStop = _map.end();
+
+	while (mapIt != mapStop)
+	{
+		std::cout << "| Date: " << mapIt->first << " | Worth: " << std::setprecition(2) << mapIt->second << " |" << std::endl;
+		mapIt++;
+	}
+}
+int
