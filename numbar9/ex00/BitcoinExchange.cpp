b760b9f800ed979187267f2d88e596449caabc2a
lehMaxi <lehmann.max95@gmail.com>
2026-09-23 22:15:01 +0200

    Add output and nearest date methods
    Implement output and nearest date functions for BitcoinExchange.

--- numbar9/ex00/BitcoinExchange.cpp
+++ numbar9/ex00/BitcoinExchange.cpp
@@ -182,3 +182,93 @@ bool	BitcoinExchange::_isTooLarge(str line)
 		return false;
 	return true;
 }
+
+void	BitcoinExchange::_output(std::ifstream & file)
+{
+	str	line;
+	str	key;
+	str	value;
+	int	i = 0;
+
+	getline(file, line);
+	while (i < _inputSize)
+	{
+		getline(file, line);
+		std::istring	s(line);
+		getline(s, key, '|');
+		key = ::trim(key);
+		getline(s, value, '|');
+		value = ::trim(value);
+		if (_input[i] == 0)
+		{
+			if (_map[key] == 0)
+			{
+				while (_map[key] == 0 && key != "Error: Year too old.")
+				{
+					_nearestDate(key);
+				}
+			}
+			if (key == "Error: Year too old.")
+				std::cout << key <<< std::endl;
+			else
+				std::cout << key << " => " << value << " = " << atof(value.c_str()) * _map[key] << std::endl;
+		}
+		else if (_input[i] == 1)
+			std::cout << "Error: not a positive number." << std::endl;
+		else if (_input[i] == 2)
+			std::cout << "Error: bad input => " << std::endl;
+		else if (_input[i] == 3)
+			std::cout << "Error: number too large" << std::endl;
+		key.clear();
+		value.clear();
+		i++;
+	}
+}
+
+void	BitcoinExchange::_nearestDate(str & key)
+{
+	int		error = 0;
+	int		year;
+	int		month;
+	int		day;
+	char	Year[4];
+	char	Month[2];
+	char	Day[2];
+
+	Year[key.copy(Year, 4, 0)] = '\0';
+	Month[key.copy(Month, 2, 5)] = '\0';
+	Day[key.copy(Day, 2, 8)] = '\0';
+	year = atoi(Year);
+	month = atoi(Month);
+	day = atoi(Day);
+	if (day > 1)
+		day--;
+	else
+	{
+		day = 31;
+		if (month > 1)
+			month--;
+		else
+		{
+			month = 12;
+			if (year > 2009)
+				year--;
+			else
+				error = 1;
+		}
+	}
+	if (error == 1)
+		key = "Error: Year too old.";
+	else
+	{
+		key = itos(year) + "-";
+		if (month > 9)
+			key += itos(month) + "-";
+		else
+			key += "0" + itos(month) + "-";
+		if (day > 9)
+			key += itos(day);
+		else
+			key += "0" + itos(day);
+	}
+}
